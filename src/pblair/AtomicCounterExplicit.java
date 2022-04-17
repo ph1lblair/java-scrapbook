e2ccc1e4012c4b8a010bf2d706858e7d9a2c7b77
Phil Blair <ph1lblair@yahoo.co.uk>
2022-04-17 15:47:55 +0100

    Add unsafe AtomicCounter

--- src/pblair/AtomicCounterExplicit.java
+++ src/pblair/AtomicCounterExplicit.java
@@ -1,5 +1,8 @@
 package pblair;
 
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -8,15 +11,52 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class AtomicCounter {
+public class AtomicCounterExplicit {
+    private static class MyAtomicInteger extends AtomicInteger {
+        private static Unsafe unsafe;
+        static {
+            Field unsafeField;
+            try {
+                unsafeField = Unsafe.class.getDeclaredField("theUnsafe");
+                unsafeField.setAccessible(true);
+                unsafe = (Unsafe) unsafeField.get(null);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        private AtomicInteger counter = new AtomicInteger(0);
 
-    private static AtomicInteger counter = new AtomicInteger(0);
+        public MyAtomicInteger(int counter) {
+            super(counter);
+        }
+
+        public int myIncrementAndGet() {
+            long valueOffset = 0L;
+            try {
+                valueOffset = unsafe.objectFieldOffset(AtomicInteger.class.getDeclaredField("value"));
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+            }
+            int v;
+            do {
+                v = unsafe.getIntVolatile(this, valueOffset);
+                counter.incrementAndGet();
+            } while (!unsafe.compareAndSwapInt(this,valueOffset, v, v + 1));
+            return v;
+        }
+
+        public int getIncrements() {
+            return counter.get();
+        }
+    }
+    private static MyAtomicInteger counter = new MyAtomicInteger(0);
+
     public static void main(String[] args) {
         class Incrementer implements Runnable {
             @Override
             public void run() {
                 for (int i = 0; i < 1_000; i++) {
-                    counter.incrementAndGet();
+                    counter.myIncrementAndGet();
                 }
             }
         }
@@ -47,9 +87,10 @@ public class AtomicCounter {
             });
 
             System.out.println("counter = " + counter);
+            System.out.println("increments = " + counter.getIncrements());
         } finally {
             service.shutdown();
         }
-
     }
+
 }
